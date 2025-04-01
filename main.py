a11ab14404004ab2aa88c91ab50a2e9bd54efa8a
yusnivtr <huuvinhkelvin@gmail.com>
2025-04-01 22:17:26 +0700

    update Word2VecMLP model

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import pandas as pd
 from dataLoader import TwitterDataset
 from processor import TextProcessor
 from model.tfidf_model import TfIdfModel
+from nltk import word_tokenize
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 train_loader = TwitterDataset('data/twitter_training.csv')
 train_loader.load_data()
@@ -19,6 +21,13 @@ preprocessor = TextProcessor()
 df_train,df_test = preprocessor.preproces_dataframe(df_train),preprocessor.preproces_dataframe(df_test)
 
 # TFIDF - Model
-tfidf_model = TfIdfModel()
-tfidf_model.train(df_train['clean text'],df_train['label'])
-tfidf_model.evaluate(df_test['clean text'],df_test['label'])
+# tfidf_model = TfIdfModel()
+# tfidf_model.train(df_train['clean text'],df_train['label'])
+# tfidf_model.evaluate(df_test['clean text'],df_test['label'])
+
+df_train["tokenized"] = df_train["clean text"].apply(lambda x: word_tokenize(x.lower()))
+df_test['tokenized'] = df_test['clean text'].apply(lambda x: word_tokenize(x.lower()))
+
+X_train,y_train = df_train['tokenized'].tolist(),df_train['label'].tolist()
+X_test,y_test = df_test['tokenized'].tolist(),df_test['label'].tolist()
+
--- model/word2vec.py
+++ model/word2vec.py
@@ -1,5 +1,8 @@
 import torch
+import numpy as np
 import torch.nn as nn
+from gensim.models import Word2Vec
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 class MLPClassifier(nn.Module):
     def __init__(self, input_size,hidden_units=256,output_size=4):
@@ -18,6 +21,44 @@ class MLPClassifier(nn.Module):
         x = self.fc3(x)
         return x
     
+class Word2VecMLP:
+    def __init__(self,vector_size=300,min_count=1,window=5,word2vec_epochs=10,mlp_epochs=10,lr=0.001):
+        self.vector_size = vector_size
+        self.min_count = min_count
+        self.window = window
+        self.word2vec = None
+        self.word2vec_epochs = word2vec_epochs
+        self.mlp_epochs = mlp_epochs
+        
+        # MLP Classifier
+        self.model = MLPClassifier(input_size=vector_size,output_size=4)
+        self.criterion = nn.CrossEntropyLoss()
+        self.optimizer = torch.optim.Adam(params=self.model.parameters(),lr=self.lr)
+        
+    def train_word2vec(self,sentences):
+        print("Training Word2Vec for", self.word2vec_epochs, "epochs...")
+        self.word2vec = Word2Vec(sentences,vector_size=self.vector_size,window=5,min_count=1,workers=4,epochs=self.word2vec_epochs)
+        
+    def compute_tfidf_weights(self,corpus):
+        vectorizer = TfidfVectorizer(tokenizer=lambda x :x,lowercase=False,preprocessor=lambda x : x)
+        vectorizer.fit(corpus)
+        return {word:vectorizer.idf_[i] for word, i in vectorizer.vocabulary_.items()}
+    
+    def tweet2vec(self,tweet,tfidf_weights):
+        words = [word for word in tweet if word in self.word2vec.wv]
+        if not words:
+            return np.zeros(self.vector_size)
+        
+        # Apply TF-IDF
+        word_vectors = np.array([self.word2vec.wv[word]*tfidf_weights.get(word) for word in words])
+        return np.mean(word_vectors,axis=0) # size = vector_size
+    
+    
+    
+    
+        
+    
+        
 
         
         
